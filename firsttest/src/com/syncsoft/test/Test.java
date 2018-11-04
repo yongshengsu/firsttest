e0ef087464bd4ad3f6212617c811d2e38d504a42
yongshengsu <545791569@qq.com>
2018-11-04 17:16:51 +0800

    初始化
    初始化提交

---
+++ firsttest/src/com/syncsoft/test/Test.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.syncsoft.test;
+
+/**
+ * 
+ * @author suyongsheng
+ * @date 2018年11月4日 下午5:02:36
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String somthingMsg = new String("Hello everyone ,this is my first time using github to submit a project ");
+		System.out.println(somthingMsg);
+	}
+
+}
